0eb1a3a311655814e6f3e499fa8645211a0e9a78
Ruslan Spanchak <105106008+RuslanSpanchak@users.noreply.github.com>
2022-06-08 00:26:01 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ tippy('#donate_btn', {
 tippy('#monobank', {
 
     content: 'MonoBank',
-    placement: 'left',
+    placement: 'bottom',
     arrow: false,
     duration: 500,
     allowHTML: true,
@@ -25,7 +25,19 @@ tippy('#monobank', {
 tippy('#privatebank', {
 
     content: '<span style="color: limegreen">ПриватБанк</span>',
-    placement: 'right',
+    placement: 'bottom',
+    arrow: false,
+    duration: 500,
+    allowHTML: true,
+    animation: "scale",
+    theme: "material",
+
+});
+
+tippy('#qr_code', {
+
+    content: '<span style="color: #536dfe">QR - код</span>',
+    placement: 'bottom',
     arrow: false,
     duration: 500,
     allowHTML: true,
@@ -49,5 +61,23 @@ $("#popup_close").click(function() {
     $("#popup").css("visibility", "hidden")
     $(".popup_body").css("opacity", "0")
     $(".popup_body").css("transform", "translate(0px, -100%)")
+
+})
+
+$("#qr_code").click(function() {
+
+    $("#popup_qr_code").css("opacity", "1")
+    $("#popup_qr_code").css("visibility", "visible")
+    $(".popup_body_qr_code").css("opacity", "1")
+    $(".popup_body_qr_code").css("transform", "translate(0px, 0px)")
+
+})
+
+$("#popup_close_qr_code").click(function() {
+
+    $("#popup_qr_code").css("opacity", "0")
+    $("#popup_qr_code").css("visibility", "hidden")
+    $(".popup_body_qr_code").css("opacity", "0")
+    $(".popup_body_qr_code").css("transform", "translate(0px, -100%)")
 
 })
